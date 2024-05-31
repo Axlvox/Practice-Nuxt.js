276f48bc3e1a123faaab9818da7fa1ede859b46d
Axlvox <leandro.jlds@gmail.com>
2024-05-31 15:22:59 -0300

    Icons personalized

--- components/Header.vue
+++ components/Header.vue
@@ -1,7 +1,18 @@
+<script setup lang="ts">
+</script>
+
 <template>
 <div>
     <h1>Header</h1>
-    <PhosphorIconAxe :size="32" color="blue" weight="fill" />
-
+    <PhosphorIconBiohazard :size="32" color="black" id="hazard"/>
+    <PhosphorIconSkull :size="32" color="gray"/>
+    <PhosphorIconBiohazard :size="32" color="black" id="hazard"/>
 </div>
 </template>
+
+<style scoped>
+#hazard {
+    background-color: yellowgreen;
+    border-radius: 10px;
+}
+</style>
