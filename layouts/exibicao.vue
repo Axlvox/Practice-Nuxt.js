5795286db6443d9c156f1b274926cbc93347153f
Axlvox <leandro.jlds@gmail.com>
2024-05-31 14:14:26 -0300

    Layout applied

---
+++ layouts/exibicao.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        Layout Exibição
+        <NuxtLink to="/">
+            Voltar
+        </NuxtLink>
+        <slot></slot>
+        Rodapé Exibição
+    </div>
+</template>
