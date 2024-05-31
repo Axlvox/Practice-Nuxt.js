f038bfd5206c14e4f6393fa2794d3f2c1e1ea0fe
Axlvox <leandro.jlds@gmail.com>
2024-05-31 16:59:59 -0300

    Add composables

--- components/Header.vue
+++ components/Header.vue
@@ -5,14 +5,14 @@
 <div>
     <h1>Header</h1>
     <PhosphorIconBiohazard :size="32" color="black" id="hazard"/>
-    <PhosphorIconSkull :size="32" color="gray"/>
+    <PhosphorIconSkull :size="32" color="gray" weight="duotone"/>
     <PhosphorIconBiohazard :size="32" color="black" id="hazard"/>
 </div>
 </template>
 
 <style scoped>
 #hazard {
-    background-color: yellowgreen;
+    background-color: yellow;
     border-radius: 10px;
-}
+    }
 </style>
--- components/video/Detalhes.vue
+++ components/video/Detalhes.vue
@@ -1,3 +1,12 @@
 <template>
     <h1>Componentes - Nuxt 3</h1>
+    <p> {{ diferencaEmDias }}</p>
 </template>
+
+<script setup lang="ts">
+const { diasPassados, diferencaEmDias } = useFormatacao()
+
+const dataVideo = new Date(2023, 9, 2);
+
+diasPassados(dataVideo);
+</script>
